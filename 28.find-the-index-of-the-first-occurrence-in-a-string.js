db75c8c62f4f4c1e58c5784a3c71811ad02a0c41
fortune4-ravishankarpal <ravishankar.pal@fortune4.in>
2024-07-21 23:03:19 +0530

    work on sorted list

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    if (haystack.includes(needle)) {
+        for (Alphabet in haystack) {
+
+
+        }
+    } else {
+        return -1
+    }
+};
+// @lc code=end
+strStr("sadbutsad", "sad")
+
+/* 
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
---
+++ 88.merge-sorted-array.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=88 lang=javascript
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+
+};
+// @lc code=end
+
+console.log(merge(
+    [1, 2, 3, 0, 0, 0],
+    3,
+    [2, 5, 6],
+    3
+))
+
+/* 
+
+
+
+
+
+
+
+
+
+
+
+ */
