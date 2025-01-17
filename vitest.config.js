dcd40c451789693444010afc7030a866107ad83d
RAVISHANKAR PAL <65111478+iravishankarpal@users.noreply.github.com>
2025-01-17 09:51:10 +0530

    delted som file

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-// https://vitest.dev/config/
-export default defineConfig({
-    test: {
-        global: true,
-        setupFiles: './vitest.setup.js',
-    },
-});
--- vitest.setup.js
+++
@@ -1,8 +0,0 @@
-import { expect, test, describe, it } from 'vitest';
-
-global.expect = expect;
-global.test = test;
-global.describe = describe;
-global.it = it;
-
-global.__dirname = __dirname;
